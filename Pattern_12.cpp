1fe1557afa18d7634f8d6675df6ea02c4abc620c
Prakul Khare <khareprakul@gmail.com>
2024-10-07 20:21:08 +0530

    Add files via upload

---
+++ Pattern_12.cpp
@@ -0,0 +1,24 @@
+// 1
+// 0 0
+// 1 1 1
+// 0 0 0 0
+// 1 1 1 1 1
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        int start=1;
+        if(i%2==0){
+            start=1;
+        }
+        else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
